2e2277f43fbf382d9b21b198354b8833dd555a99
Marcelo de Oliveira <marcelo_rde@yahoo.com.br>
2020-03-29 20:53:32 -0300

    Backend Finalizado

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,18 @@
 const express = require('express');
 
 const OngController = require('./constrollers/OngController');
+const ProfileController = require('./constrollers/ProfileController');
 const IncidentController = require('./constrollers/IncidentController');
+const SessionController = require('./constrollers/SessionController');
 
 const routes = express.Router();
 
+routes.post('/session', SessionController.index);
+
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
+
+routes.get('/profile', ProfileController.index);
 
 routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
